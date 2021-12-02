b2cac3e1fe1b0356cd57b88c7a8ffc1c0076f958
peterpetrekanics <peter.petrekanics@liferay.com>
2021-12-02 16:21:30 +0100

    FileReader second test added

--- week08-lab-tasks/src/test/java/day04/FileReaderTest.java
+++ week08-lab-tasks/src/test/java/day04/FileReaderTest.java
@@ -14,4 +14,11 @@ class FileReaderTest {
 	assertEquals(14, fr.findSmallestTemperatureSpread("src/main/resources/weather.dat"));
 
 	}
+
+	@Test
+	void testFileReaderFileNotExists(){
+		IllegalStateException err = assertThrows(IllegalStateException.class,
+				  () -> new FileReader().findSmallestTemperatureSpread("filename"));
+		assertEquals("Can not read file.", err.getMessage());
+	}
 }
